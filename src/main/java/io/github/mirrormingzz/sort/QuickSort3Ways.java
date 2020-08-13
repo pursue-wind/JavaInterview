be408544418fc59fb9197a6eddcb0721664c355e
mirrorming <mirrorming@qq.com>
2020-08-14 00:10:35 +0800

    三路快排✔✔✔

--- src/main/java/io/github/mirrormingzz/sort/QuickSort3Ways.java
+++ src/main/java/io/github/mirrormingzz/sort/QuickSort3Ways.java
@@ -24,6 +24,48 @@ public class QuickSort3Ways {
 
         int v = arr[l];
         while (i < gt) {
+            if (arr[i] < v)
+                swap(arr, i++, ++lt);
+            else if (arr[i] > v)
+                swap(arr, --gt, i);
+            else //if (arr[i] == v)
+                i++;
+        }
+
+        swap(arr, l, lt);
+        quickSort3Ways(arr, l, lt - 1);
+        quickSort3Ways(arr, gt, r);
+    }
+
+    private static void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 8, 6, 2, 1, 9, 5, 7, 4};
+//        quickSort3Ways(arr);
+        quickSort3WaysWithPrint(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void quickSort3WaysWithPrint(int[] arr) {
+        quickSort3WaysWithPrint(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort3WaysWithPrint(int[] arr, int l, int r) {
+        if (l >= r) return;
+
+        //arr[l ... lt] < v
+        int lt = l;
+        //arr[gt ... r] > v
+        int gt = r + 1;
+        // arr[lt ... i) ==v
+        int i = l + 1;
+
+        int v = arr[l];
+        while (i < gt) {
             if (arr[i] < v) {
                 printIPos(i, arr, lt, gt);
                 System.err.println(arr[i] + "小于" + v + "     交换 " + arr[i] + " <--> " + arr[(lt + 1)]);
@@ -45,6 +87,9 @@ public class QuickSort3Ways {
         quickSort3Ways(arr, gt, r);
     }
 
+    /**
+     * 打印 指针位置
+     */
     private static void printIPos(int i, int[] arr, int lt, int gt) {
         System.err.println("============================================");
         System.err.print("i 位置： ");
@@ -69,15 +114,4 @@ public class QuickSort3Ways {
         System.err.println("============================================");
     }
 
-    private static void swap(int[] arr, int x, int y) {
-        int temp = arr[x];
-        arr[x] = arr[y];
-        arr[y] = temp;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {3, 8, 6, 2, 1, 9, 5, 7, 4};
-        quickSort3Ways(arr);
-        System.out.println(Arrays.toString(arr));
-    }
 }
