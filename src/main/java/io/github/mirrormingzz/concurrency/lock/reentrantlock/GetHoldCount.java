6bae12cf4d48689ca6effb72879aaeee8eef661e
mirrorming <mirrorming@qq.com>
2020-09-07 00:09:32 +0800

    juc -> lock✔✔✔

---
+++ src/main/java/io/github/mirrormingzz/concurrency/lock/reentrantlock/GetHoldCount.java
@@ -0,0 +1,28 @@
+package io.github.mirrormingzz.concurrency.lock.reentrantlock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * TODO
+ *
+ * @author Mireal
+ */
+public class GetHoldCount {
+    private static ReentrantLock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        System.out.println(lock.getHoldCount());
+        lock.lock();
+        System.out.println(lock.getHoldCount());
+        lock.lock();
+        System.out.println(lock.getHoldCount());
+        lock.lock();
+        System.out.println(lock.getHoldCount());
+        lock.unlock();
+        System.out.println(lock.getHoldCount());
+        lock.unlock();
+        System.out.println(lock.getHoldCount());
+        lock.unlock();
+        System.out.println(lock.getHoldCount());
+    }
+}
